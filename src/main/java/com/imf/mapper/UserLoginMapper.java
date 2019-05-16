f7de261e6885ce195c4629dee28e0d220a4011d8
jixiang <252632330@qq.com>
2019-05-16 16:59:38 +0800

    增加新表

---
+++ src/main/java/com/imf/mapper/UserLoginMapper.java
@@ -0,0 +1,31 @@
+package com.imf.mapper;
+
+import com.imf.pojo.UserLogin;
+import com.imf.pojo.UserLoginExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserLoginMapper {
+    int countByExample(UserLoginExample example);
+
+    int deleteByExample(UserLoginExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserLogin record);
+
+    int insertSelective(UserLogin record);
+
+    List<UserLogin> selectByExample(UserLoginExample example);
+
+    UserLogin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") UserLogin record, @Param("example") UserLoginExample example);
+
+    int updateByExample(@Param("record") UserLogin record, @Param("example") UserLoginExample example);
+
+    int updateByPrimaryKeySelective(UserLogin record);
+
+    int updateByPrimaryKey(UserLogin record);
+}
---
+++ src/main/java/com/imf/pojo/UserLogin.java
@@ -0,0 +1,65 @@
+package com.imf.pojo;
+
+import java.util.Date;
+
+public class UserLogin {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String lastLoginIp;
+
+    private Date lastLoginTime;
+
+    private Date registTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp == null ? null : lastLoginIp.trim();
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public Date getRegistTime() {
+        return registTime;
+    }
+
+    public void setRegistTime(Date registTime) {
+        this.registTime = registTime;
+    }
+}
